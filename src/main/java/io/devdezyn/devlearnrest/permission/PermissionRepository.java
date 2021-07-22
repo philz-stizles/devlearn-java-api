f9a80fa346c87db35d4c48a78e1ca4c8a727f967
philz-stizles <theophilusighalo@gmail.com>
2021-07-22 15:34:44 +0100

    Init jwt auth

--- src/main/java/io/devdezyn/devlearnrest/permission/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package io.devdezyn.devlearnrest.permission;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PermissionRepository extends JpaRepository<Permission, Long> {
-    public Permission getPermissionByName(String name);
-}

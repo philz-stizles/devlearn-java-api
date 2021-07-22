018eb442676da443b245564b9d5ab277df50c604
philz-stizles <theophilusighalo@gmail.com>
2021-07-22 17:12:23 +0100

    configureroles and permissions

--- src/main/java/io/devdezyn/devlearnrest/security/ApplicationSecurityConfig.java
+++ src/main/java/io/devdezyn/devlearnrest/security/ApplicationSecurityConfig.java
@@ -1,50 +1,65 @@
 package io.devdezyn.devlearnrest.security;
 
-import io.devdezyn.devlearnrest.auth.ApplicationUserService;
-import io.devdezyn.devlearnrest.jwt.JwtConfig;
-import io.devdezyn.devlearnrest.jwt.JwtTokenVerifierFilter;
-import io.devdezyn.devlearnrest.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import javax.crypto.SecretKey;
+import static io.devdezyn.devlearnrest.security.ApplicationUserRole.*;
+import static io.devdezyn.devlearnrest.security.ApplicationUserPermission.*;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
     private final PasswordEncoder passwordEncoder;
-    private ApplicationUserService applicationUserService;
-    private final JwtConfig jwtConfig;
-    private final SecretKey secretKey;
 
     @Autowired
-    public ApplicationSecurityConfig(
-            PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService,
-            JwtConfig jwtConfig, SecretKey secretKey) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
         this.passwordEncoder = passwordEncoder;
-        this.applicationUserService = applicationUserService;
-        this.jwtConfig = jwtConfig;
-        this.secretKey = secretKey;
     }
 
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("philz")
+                .password(passwordEncoder.encode("p@ssw0rd"))
+//                .roles(ADMIN.name()) // ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+
+        UserDetails linda = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("p@ssw0rd"))
+                .roles(STUDENT.name()) // ROLE_USER
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, linda);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
-                .addFilterAfter(new JwtTokenVerifierFilter(jwtConfig, secretKey), JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
-//                .antMatchers("/api/**").hasRole()
+                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name())
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 }
+
---
+++ src/main/java/io/devdezyn/devlearnrest/user/UserController.java
@@ -0,0 +1,7 @@
+package io.devdezyn.devlearnrest.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
